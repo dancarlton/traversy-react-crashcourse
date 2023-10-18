dde17402a89b229f98926e09bb79ee5b6a92298f
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-18 15:49:11 -0700

    implement backend

--- src/App.js
+++ src/App.js
@@ -6,26 +6,7 @@ import AddTask from './components/AddTask'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30',
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([])
 
   // Add Task
   const addTask = task => {
