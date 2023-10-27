825671c8938d3ee883cc3c66fc0e6134b80c2ebd
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-27 13:11:46 -0700

    completed the UI portion. next thing to do is backend stuff. timestape = 1:20:36

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  const onClick = () => {
+    console.log('Button Clicked')
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
-  );
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
