6e74ff713d4194ed4b0269f3e73302d8bbd30964
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-31 16:14:50 -0700

    Delete Tasks implemented

--- src/App.js
+++ src/App.js
@@ -33,7 +33,11 @@ const App = () => {
   }
 
   // Delete Task
-  const deleteTask = id => {
+  const deleteTask = async id => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter(task => task.id !== id))
   }
 
