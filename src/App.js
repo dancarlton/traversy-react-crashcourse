d443ae91272d5e4c8122b3733a0bdcb655308cd2
Dan Carlton <danlouis.carlton@gmail.com>
2023-11-02 16:22:14 -0700

    Add Task from backend

--- src/App.js
+++ src/App.js
@@ -26,10 +26,22 @@ const App = () => {
   }
 
   // Add Task
-  const addTask = task => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async task => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
